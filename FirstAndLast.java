4f5430b2d1f719d4626a674d00351255db4ee856
rudrarapu-kamal <85126839+rudrarapu-kamal@users.noreply.github.com>
2022-05-11 21:19:27 +0530

    Update FirstAndLast.java

--- FirstAndLast.java
+++ FirstAndLast.java
@@ -1,4 +1,4 @@
-CLICK HERE FOR [Problem link](https:leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/)
+https:leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 /*Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
 
 If target is not found in the array, return [-1, -1].
