2e35491a3ca0349f0521a1422e7917858c886e71
rudrarapu-kamal <85126839+rudrarapu-kamal@users.noreply.github.com>
2022-05-11 20:48:32 +0530

    Product Array Puzzle

--- ProductArrayPuzzle.java
+++ ProductArrayPuzzle.java
@@ -1,7 +1,7 @@
 //problem link:https://leetcode.com/problems/product-of-array-except-self/
 /*Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
 
-The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer
 
 You must write an algorithm that runs in O(n) time and without using the division operation.
 
